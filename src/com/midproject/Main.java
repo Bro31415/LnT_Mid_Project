3cb3a10463f1c0c0740f32aa64a922630ad836c3
Bro3.14 <ln060303@gmail.com>
2022-02-18 00:13:14 +0700

    Made the framework of the menu

---
+++ src/com/midproject/Main.java
@@ -0,0 +1,22 @@
+package com.midproject;
+
+import java.util.scanner
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("1. Insert Data Karyawan");
+        System.out.println("2. View Data Karyawan");
+        System.out.println("3. Update Data Karyawan");
+        System.out.println("4. Delete Data Karyawan");
+
+
+
+
+
+
+    }
+
+
+
+}
